e274b89589f3beeb023cf6d1f2bc75c7c7eefc47
Aju1999 <ajaypt1942@gmail.com>
2023-11-21 18:35:57 +0530

    Css changes

--- src/app/listuser/listuser.component.ts
+++ src/app/listuser/listuser.component.ts
@@ -1,33 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
-
+import { SessionStorageService } from 'ngx-webstorage';
+ 
 @Component({
   selector: 'app-listuser',
   templateUrl: './listuser.component.html',
   styleUrls: ['./listuser.component.css']
 })
-export class ListuserComponent implements OnInit
- {users:any;
-  constructor(private route:Router,private userService:UserService)
-  {
-
+export class ListuserComponent implements OnInit {
+  users: any;
+  selectedStandard: string | undefined;
+ 
+  constructor(private route: Router, private userService: UserService ,    private loacalStorage:SessionStorageService) {}
+ 
+  ngOnInit(): void {    
+    this.selectedStandard =
+    this.loacalStorage.retrieve('currentuser').standards[0].name;; // Replace with your logic to get the teacher's standard
+    this.getUsers();
   }
-  ngOnInit(): void 
-  {
-    this.userService.getAllStudents().subscribe(
-     (data:any)=>
-     {
-      console.log("data retrived succesfully");
-       this.users=data;
-     }
-    );
-    
+ 
+  getUsers(): void {
+    if (this.selectedStandard) {
+      this.userService.getUsersByStandard(this.selectedStandard).subscribe(
+        (data: any) => {
+          console.log("data retrieved successfully");
+          this.users = data;
+        }
+      );
+    } else {
+      console.error("Selected standard is undefined.");
+    }
   }
-public back()
-{
-  this.route.navigate([ 'students' ]);
+ 
+  public back() {
+    this.route.navigate(['students']);
+  }
 }
-
-
-}
+ 
